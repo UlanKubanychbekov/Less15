ebb7b90941108b97b8d7317dd6819f9e372a7651
user <ulanskii03@gmail.com>
2022-02-07 16:52:41 +0600

    Initial commit

---
+++ src/peaksoft/Main.java
@@ -0,0 +1,13 @@
+package peaksoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Class clasS = new Class(24, "Ayjan eje", new int[] {24,45,67,879});
+        System.out.println(clasS.getWord()+" "+clasS.getSan()+" :");
+        clasS.getArray();
+
+
+    }
+}
